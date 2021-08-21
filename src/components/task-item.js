077b541d451645440d24976d91b50b3abcd7ba01
Patrick Martinus <trickmartpat77@gmail.com>
2021-08-21 19:58:48 +0700

    Now we can add new item

--- src/components/task-item.js
+++ src/components/task-item.js
@@ -3,33 +3,30 @@ class TaskItem extends HTMLElement {
     super();
   }
 
-  connectedCallback() {
+  set data(items) {
+    this.item = items;
     this.render();
   }
 
   render() {
-    this.innerHTML = `
+    this.innerHTML += `
     <div class="task-title">
-      <h2>Manajemen Operasional</h2>
+      <h2>${this.item.lecture}</h2>
     </div>
 
     <!-- START: Task description 1 -->
     <div class="task-week">
-      <h3>Week 0</h3>
+      <h3>Week ${this.item.week}</h3>
       <div class="task-desc">
         <h4>Task Description:</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-          exercitationem illum necessitatibus quasi nobis quae animi alias
-          cumque nisi maiores.
-        </p>
+        <p>${this.item.desc}</p>
         <div class="criteria">
           <h4>Criteria:</h4>
-          <p>time here</p>
+          <p>${this.item.criteria}</p>
         </div>
         <div class="deadline">
           <h4>Deadline:</h4>
-          <p>date here</p>
+          <p>${this.item.deadline}</p>
         </div>
       </div>
     </div>`;
--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,8 @@ function main() {
   }
 
   const newItem = document.getElementById("add-new-item");
+  const list = document.querySelector("#task-list");
+  // const item = document.querySelector("task-item");
   let items = {
     lecture: "",
     week: "",
@@ -30,16 +32,21 @@ function main() {
       btnBack.style.display = "none";
 
       items.lecture = document.getElementsByName("lecture")[0].value;
+      items.week = document.getElementsByName("week")[0].value;
       items.desc = document.getElementsByName("desc")[0].value;
       items.criteria = document.getElementsByName("criteria")[0].value;
       items.deadline = document.getElementsByName("date")[0].value;
 
       document.getElementsByName("lecture")[0].value = "";
+      document.getElementsByName("week")[0].value = "";
       document.getElementsByName("desc")[0].value = "";
       document.getElementsByName("criteria")[0].value = "";
       document.getElementsByName("date")[0].value = "yyyy-MM-dd";
-
       console.log(items);
+
+      const newItem = document.createElement("task-item");
+      newItem.data = items;
+      list.appendChild(newItem);
     };
   }
 }
