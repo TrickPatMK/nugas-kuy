a6a3d1ffd865726d162acc767fdb9a07a1e368ed
Patrick Martinus <trickmartpat77@gmail.com>
2021-09-22 02:02:19 +0700

    Simplyfied the item object class

--- src/components/task-item.js
+++ src/components/task-item.js
@@ -1,6 +1,11 @@
 class TaskItem extends HTMLElement {
   constructor() {
     super();
+    this.lecture = "";
+    this.week = "";
+    this.desc = "";
+    this.criteria = "";
+    this.deadline = "";
   }
 
   set data(items) {
@@ -8,25 +13,34 @@ class TaskItem extends HTMLElement {
     this.render();
   }
 
+  addItem(lecture, week, desc, criteria, deadline) {
+    this.lecture = lecture;
+    this.week = week;
+    this.desc = desc;
+    this.criteria = criteria;
+    this.deadline = deadline;
+    this.render();
+  }
+
   render() {
     this.innerHTML += `
     <div class="task-title">
-      <h2>${this.item.lecture}</h2>
+      <h2>${this.item.lecture || this.lecture}</h2>
     </div>
 
     <!-- START: Task description 1 -->
     <div class="task-week">
-      <h3>Week ${this.item.week}</h3>
+      <h3>Week ${this.item.week || this.week}</h3>
       <div class="task-desc">
         <h4>Task Description:</h4>
-        <p>${this.item.desc}</p>
+        <p>${this.item.desc || this.desc}</p>
         <div class="criteria">
           <h4>Criteria:</h4>
-          <p>${this.item.criteria}</p>
+          <p>${this.item.criteria || this.criteria}</p>
         </div>
         <div class="deadline">
           <h4>Deadline:</h4>
-          <p>${this.item.deadline}</p>
+          <p>${this.item.deadline || this.deadline}</p>
         </div>
       </div>
     </div>`;
