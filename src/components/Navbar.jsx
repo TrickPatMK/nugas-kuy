e57f7783ec122970e4b2e2fe7c04eb4b7613422c
Patrick Martinus <trickmartpat77@gmail.com>
2022-01-02 20:00:08 +0700

    update: apply simple routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,22 @@ import "material-icons/iconfont/filled.css";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  function onClickHandler(e) {
+
+    const navItems = document.querySelectorAll(".nav-item");
+    navItems.forEach(item => item.classList.remove("active"))
+    e.currentTarget.classList.toggle("active")
+  }
+  
   return (
     <>
       <nav className="nav-wrapper">
-        <Link to='/' className="nav-item">
-        <span class="material-icons">home</span>
+        <Link to='/' className="nav-item active" onClick={onClickHandler}>
+          <span className="material-icons">home</span>
           <span className="nav-item-text">Home</span>
         </Link>
-        <Link to='/addTask' className="nav-item">
-          <span class="material-icons">add</span>
+        <Link to='/addTask' className="nav-item" onClick={onClickHandler}>
+          <span className="material-icons">add</span>
           <span className="nav-item-text">AddTask</span>
         </Link>
       </nav>
